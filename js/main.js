f96303512d7cdabc56a845d61f048cc6fccf862e
Rasoul <r.zinati1372@gmail.com>
2021-07-03 15:04:08 +0430

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Make sure sw are supported
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('../sw_cached_pages.js')
+        .then(reg => console.log('Service Worker: Registered'))
+        .catch(err => console.log(`Service Worker: Error: ${err}`));
+}
