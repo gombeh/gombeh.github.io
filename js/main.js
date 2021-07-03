d928620749197ffef2acdd77e3101e4e9d90d9bb
Rasoul <r.zinati1372@gmail.com>
2021-07-03 15:14:00 +0430

    add page to thata

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Make sure sw are supported
 if('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('../sw_cached_site.js')
+        .register('../sw_cached_pages.js')
         .then(reg => console.log('Service Worker: Registered'))
         .catch(err => console.log(`Service Worker: Error: ${err}`));
 }
